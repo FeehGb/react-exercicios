6c548e5ecb33290f51f404cc078a70b6ec1a7435
Felipe Augusto Gonçalves Basilio <43045359+FeehGb@users.noreply.github.com>
2019-08-12 22:35:02 -0300

    componente familia

--- src/components/Familia.jsx
+++ src/components/Familia.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-
+import React from 'react' 
+import  {filhosComProps}  from "./utils/Utils"
 
 export default props =>
     <div>
         {/* props.children */}
-        {React.cloneElement(props.children,
+        
+        {filhosComProps(props)}
+        
+        
+        
+        {/* React.Children.map(props.children, child => {
+            return React.cloneElement(child, { ...props})
+        }) */}
+
+        {/*React.cloneElement(props.children,
             {...props}
-        )}
+        )*/} 
             
         {/* React.cloneElement(props.children,
             {sobrenome: props.sobrenome}
--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-//import PrimeiroComponente from './components/PrimeiroComponente'
-import { CompA, CompB } from './components/DoisComponentes'
-import MultiploElements from './components/MultiploElements'
+import React                    from 'react'
+import ReactDOM                 from 'react-dom'
+//import PrimeiroComponente     from './components/PrimeiroComponente'
+//import { CompA, CompB }         from './components/DoisComponentes'
+//import MultiploElements         from './components/MultiploElements'
+import Familia                    from './components/Familia'
+import Membro                     from './components/Membro'
+
 const elemento = document.getElementById("root")
-ReactDOM.render(
+ReactDOM.render (
     <div>
-        <MultiploElements />
+        
+        <Familia sobrenome="Basilio"> 
+
+            <Membro nome="Felipe" />
+            <Membro nome="Ryan" />
+        </Familia>
+        {/* <MultiploElements />
         <CompA valor="Ola eu sou A"/>
-        <CompB valor="Ola eu sou B"/>
+        <CompB valor="Ola eu sou B"/> */}
         {/*< PrimeiroComponente value="teste" />,  */}
-    </div> 
-    
-    ,
-    
-elemento)
-   
+    </div> ,elemento )
